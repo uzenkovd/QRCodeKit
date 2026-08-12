5c71e0fad5332fe40cb2226faaa424a78394aa9e
Dmytro Uzenkov <uzenkov1.d@gmail.com>
2026-08-12 23:55:20 +0300

    Add byte and Kanji mode validation

--- Sources/QRCodeKit/Enums/EncodingMode.swift
+++ Sources/QRCodeKit/Enums/EncodingMode.swift
@@ -5,11 +5,13 @@
 //  Created by Dmytro Uzenkov on 11.08.2026.
 //
 
+import Foundation
+
 public enum EncodingMode: CaseIterable {
     case numeric
     case alphanumeric
-    case byte
     case kanji
+    case byte
     
     func canEncode(_ message: String) -> Bool {
         switch self {
@@ -19,10 +21,39 @@ public enum EncodingMode: CaseIterable {
             return message.allSatisfy {
                 AlphanumericTable.values[$0] != nil
             }
-        case .byte:
-            return false
         case .kanji:
-            return false
+            return canEncodeKanji(message)
+        case .byte:
+            return message.canBeConverted(to: .isoLatin1)
         }
+    }
+    
+    private func canEncodeKanji(_ message: String) -> Bool {
+        for character in message {
+            let string = String(character)
+            guard let data = string.data(using: .shiftJIS) else {
+                return false
+            }
+            
+            let bytes = [UInt8](data)
+            guard bytes.count == 2 else {
+                return false
+            }
+            
+            let shiftJISCode = (UInt16(bytes[0]) << 8) | UInt16(bytes[1])
+            guard isInFirstRange(shiftJISCode) || isInSecondRange(shiftJISCode) else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    private func isInFirstRange(_ shiftJISCode: UInt16) -> Bool {
+        shiftJISCode >= 0x8140 && shiftJISCode <= 0x9FFC
+    }
+    
+    private func isInSecondRange(_ shiftJISCode: UInt16) -> Bool {
+        shiftJISCode >= 0xE040 && shiftJISCode <= 0xEBBF
     }
 }
