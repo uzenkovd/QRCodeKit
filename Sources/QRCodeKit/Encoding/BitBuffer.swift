a41490ecfd2f0bac4c6ebb28225078516dff3c3c
Dmytro Uzenkov <uzenkov1.d@gmail.com>
2026-08-17 15:22:13 +0300

    feat: implement BitBuffer append

--- Sources/QRCodeKit/Encoding/BitBuffer.swift
+++ Sources/QRCodeKit/Encoding/BitBuffer.swift
@@ -6,32 +6,32 @@
 //
 
 struct BitBuffer {
-    private var bytes: [UInt8]
-    private var bitCount: Int
+    private var storage: [UInt8]
+    private var count: Int
     
-    private var byteCount: Int {
-        bytes.count
+    var isAligned: Bool {
+        count.isMultiple(of: 8)
+    }
+    
+    var byteCount: Int {
+        storage.count
     }
     
     private var lastByte: UInt8? {
-        bytes.last
-    }
-    
-    private var isAligned: Bool {
-        bitCount.isMultiple(of: 8)
+        storage.last
     }
     
     private var usedBits: Int {
-        bitCount % 8
+        count % 8
     }
     
     private var freeBits: Int {
-        (-bitCount) % 8
+        8 - usedBits
     }
     
     init() {
-        bytes = []
-        bitCount = 0
+        storage = []
+        count = 0
     }
     
     mutating func append(_ value: UInt32, bitCount: Int) {
@@ -39,19 +39,57 @@ struct BitBuffer {
         
         if bitCount < 32 {
             precondition(value < (1 << bitCount))
+        }
+        
+        var remainingBits = bitCount
+        var processedBits = 0
+        
+        while remainingBits > 0 {
+            let bitsToAppend = min(freeBits, remainingBits)
             
-            var remainingBitCount = bitCount
-            
-            while remainingBitCount > 0 {
-                if isAligned {
+            if isAligned {
+                let newByte: UInt8
+                
+                if remainingBits < 8 {
+                    let expandShift = freeBits - bitsToAppend
+                    let shiftedValue = value << expandShift
                     
+                    newByte = UInt8(shiftedValue)
                     
+                    storage.append(newByte)
                 } else {
-//                    let lastByte = self.lastByte ?? 0
-//                    let bitsToAppend = value >> (bitCount - freeBits)
-//                    bytes[byteCount - 1] = lastByte | UInt8(bitsToAppend)
+                    let extractionShift = bitCount - processedBits - bitsToAppend
+                    let shiftedValue = value >> extractionShift
+                    
+                    let mask: UInt32 = (1 << bitsToAppend) - 1
+                    let bits = shiftedValue & mask
+                    newByte = UInt8(bits)
+                    
+                    storage.append(newByte)
                 }
+            } else {
+                guard let lastByte = self.lastByte else {
+                    preconditionFailure("BitBuffer is in an invalid state")
+                }
+                
+                let extractionShift = bitCount - processedBits - bitsToAppend
+                let shiftedValue = value >> extractionShift
+                
+                let mask: UInt32 = (1 << bitsToAppend) - 1
+                let bits = shiftedValue & mask
+                
+                let placementShift = freeBits - bitsToAppend
+                let shiftedBits = bits << placementShift
+                let fullLastByte = lastByte | UInt8(shiftedBits)
+                
+                storage[byteCount - 1] = fullLastByte
             }
+            
+            processedBits += bitsToAppend
+            remainingBits -= bitsToAppend
+            count += bitsToAppend
         }
     }
 }
+
+// TODO: Extract bit extraction logic into a helper.
