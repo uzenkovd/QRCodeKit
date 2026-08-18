306979e5add360005effb9a8043571f3191f3a12
Dmytro Uzenkov <uzenkov1.d@gmail.com>
2026-08-18 22:27:24 +0300

    feat: add initial DataEncoder implementation

--- Sources/QRCodeKit/Encoding/DataEncoder.swift
+++ Sources/QRCodeKit/Encoding/DataEncoder.swift
@@ -5,14 +5,29 @@
 //  Created by Dmytro Uzenkov on 15.08.2026.
 //
 
-//struct DataEncoder {
-//    func encode(
-//        message: String,
-//        encodingMode: EncodingMode,
-//        errorCorrectionLevel: ErrorCorrectionLevel,
-//        version: QRVersion
-//    ) -> [UInt8] {
-//        
-//        return []
-//    }
-//}
+struct DataEncoder {
+    func encode(
+        _ message: String,
+        mode: EncodingMode,
+        errorCorrectionLevel: ErrorCorrectionLevel,
+        version: QRVersion
+    ) -> [UInt8] {
+        let modeIndicator = mode.indicator
+        
+        let characterCountIndicator = UInt32(message.count)
+        let characterCountIndicatorLength = CharacterCountIndicatorLengths.length(
+            for: version,
+            mode: mode
+        )
+        
+        var bitBuffer = BitBuffer()
+        
+        bitBuffer.append(modeIndicator, bitCount: 4)
+        bitBuffer.append(
+            characterCountIndicator,
+            bitCount: characterCountIndicatorLength
+        )
+        
+        return bitBuffer.bytes
+    }
+}
