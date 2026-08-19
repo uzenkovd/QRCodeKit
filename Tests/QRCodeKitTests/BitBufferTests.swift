ba01ee89f6f67dae0b87d6e2495b6dbd82a741ad
Dmytro Uzenkov <uzenkov1.d@gmail.com>
2026-08-19 17:51:59 +0300

    test: add BitBuffer contents append tests

--- Tests/QRCodeKitTests/BitBufferTests.swift
+++ Tests/QRCodeKitTests/BitBufferTests.swift
@@ -10,6 +10,9 @@ import Testing
 
 @Suite
 struct BitBufferTests {
+    
+    // MARK: - append(_:bitCount:)
+    
     @Test
     func emptyBitBuffer() {
         let buffer = BitBuffer()
@@ -182,7 +185,7 @@ struct BitBufferTests {
     }
 
     @Test
-    func appendAcrossTwoByteBoundary() {
+    func appendMultipleValuesAcrossTwoByteBoundary() {
         var buffer = BitBuffer()
         
         buffer.append(0b1001, bitCount: 6)
@@ -193,5 +196,131 @@ struct BitBufferTests {
         #expect(buffer.count == 16)
         #expect(buffer.byteCount == 2)
         #expect(buffer.isAligned)
+    }
+    
+    //MARK: - append(contentsOf:)
+    
+    @Test
+    func appendEmptyToEmptyBuffer() {
+        var buffer = BitBuffer()
+        let other = BitBuffer()
+        
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 0)
+        #expect(buffer.bytes.isEmpty)
+    }
+    
+    @Test
+    func appendEmptyToAlignedBuffer() {
+        var buffer = BitBuffer()
+        let other = BitBuffer()
+        
+        buffer.append(0b11001100, bitCount: 8)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 8)
+        #expect(buffer.bytes == [0b11001100])
+    }
+    
+    @Test
+    func appendEmptyToUnalignedBuffer() {
+        var buffer = BitBuffer()
+        let other = BitBuffer()
+        
+        buffer.append(0b101, bitCount: 3)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 3)
+        #expect(buffer.bytes == [0b10100000])
+    }
+    
+    @Test
+    func appendAlignedToEmptyBuffer() {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        other.append(0b10101010, bitCount: 8)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 8)
+        #expect(buffer.bytes == [0b10101010])
+    }
+    
+    @Test
+    func appendUnalignedToEmptyBuffer() {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        other.append(0b101, bitCount: 3)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 3)
+        #expect(buffer.bytes == [0b10100000])
+    }
+    
+    @Test
+    func appendUnalignedToAlignedBuffer() {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        buffer.append(0b11001100, bitCount: 8)
+        other.append(0b101, bitCount: 3)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 11)
+        #expect(buffer.bytes == [0b11001100, 0b10100000])
+    }
+    
+    @Test
+    func appendAlignedToUnalignedBuffer() {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        buffer.append(0b101, bitCount: 3)
+        other.append(0b11001100, bitCount: 8)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 11)
+        #expect(buffer.bytes == [0b10111001, 0b10000000])
+    }
+    
+    @Test
+    func appendUnalignedToUnalignedBuffer() {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        buffer.append(0b101, bitCount: 3)
+        other.append(0b1101, bitCount: 4)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 7)
+        #expect(buffer.bytes == [0b10111010])
+    }
+    
+    @Test
+    func appendAlignedToAlignedBuffer() {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        buffer.append(0b11001100, bitCount: 8)
+        other.append(0b00110011, bitCount: 8)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 16)
+        #expect(buffer.bytes == [0b11001100, 0b00110011])
+    }
+    
+    @Test(arguments: Array(1...32))
+    func appendBufferWithBitCounts(_ bitCount: Int) {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        let value = UInt32.max >> (32 - bitCount)
+        other.append(value, bitCount: bitCount)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == bitCount)
+        #expect(buffer.bytes == other.bytes)
     }
 }
