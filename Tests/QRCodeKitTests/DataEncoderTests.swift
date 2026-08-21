a9620842c49e25be19432a6288303bf8231fd07f
Dmytro Uzenkov <uzenkov1.d@gmail.com>
2026-08-21 22:56:20 +0300

    test: add alphanumeric data encoding tests

--- Tests/QRCodeKitTests/DataEncoderTests.swift
+++ Tests/QRCodeKitTests/DataEncoderTests.swift
@@ -60,7 +60,11 @@ struct DataEncoderTests {
         let result = encoder.encodeData("12345", mode: .numeric)
         
         #expect(result.count == 17)
-        #expect(result.bytes == [0b00011110, 0b11010110, 0b10000000])
+        #expect(result.bytes == [
+            0b00011110,
+            0b11010110,
+            0b10000000
+        ])
     }
     
     @Test
@@ -70,7 +74,11 @@ struct DataEncoderTests {
         let result = encoder.encodeData("123456", mode: .numeric)
         
         #expect(result.count == 20)
-        #expect(result.bytes == [0b00011110, 0b11011100, 0b10000000])
+        #expect(result.bytes == [
+            0b00011110,
+            0b11011100,
+            0b10000000
+        ])
     }
     @Test
     func encodeNumericMultipleGroupsWithRemainder() {
@@ -106,5 +114,90 @@ struct DataEncoderTests {
         
         #expect(result.count == 10)
         #expect(result.bytes == [0b01111110, 0b11000000])
+    }
+    
+    //MARK: - encodeData(_:mode:) - Alphaumeric
+    
+    @Test
+    func encodeAlphanumericSingleCharacter() {
+        let encoder = DataEncoder()
+        
+        let result = encoder.encodeData("A", mode: .alphanumeric)
+        
+        #expect(result.count == 6)
+        #expect(result.bytes == [0b00101000])
+    }
+    
+    @Test
+    func encodeAlphanumericTwoCharacters() {
+        let encoder = DataEncoder()
+        
+        let result = encoder.encodeData("AZ", mode: .alphanumeric)
+        
+        #expect(result.count == 11)
+        #expect(result.bytes == [0b00111100, 0b10100000])
+    }
+    
+    @Test
+    func encodeAlphanumericThreeCharacters() {
+        let encoder = DataEncoder()
+        
+        let result = encoder.encodeData("09A", mode: .alphanumeric)
+        
+        #expect(result.count == 17)
+        #expect(result.bytes == [
+            0b00000001,
+            0b00100101,
+            0b00000000
+        ])
+    }
+    
+    @Test
+    func encodeAlphanumericFourCharacters() {
+        let encoder = DataEncoder()
+        
+        let result = encoder.encodeData("9AZ:", mode: .alphanumeric)
+        
+        #expect(result.count == 22)
+        #expect(result.bytes == [
+            0b00110011,
+            0b11111001,
+            0b01001100
+        ])
+    }
+    
+    @Test
+    func encodeAlphanumericFiveCharacters() {
+        let encoder = DataEncoder()
+        
+        let result = encoder.encodeData("Z *./", mode: .alphanumeric)
+        
+        #expect(result.count == 28)
+        #expect(result.bytes == [
+            0b11001001,
+            0b01111100,
+            0b00010110,
+            0b10110000
+        ])
+    }
+    
+    @Test
+    func encodeAlphanumericSingleDigit() {
+        let encoder = DataEncoder()
+        
+        let result = encoder.encodeData("0", mode: .alphanumeric)
+        
+        #expect(result.count == 6)
+        #expect(result.bytes == [0b00000000])
+    }
+    
+    @Test
+    func encodeAlphanumericMaximumPair() {
+        let encoder = DataEncoder()
+        
+        let result = encoder.encodeData("::", mode: .alphanumeric)
+        
+        #expect(result.count == 11)
+        #expect(result.bytes == [0b11111101, 0b00000000])
     }
 }
