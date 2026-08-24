456802be983a79ec07148b3327a3fc531af7505e
Dmytro Uzenkov <uzenkov1.d@gmail.com>
2026-08-24 18:28:00 +0300

    test: improve bit buffer coverage

--- Tests/QRCodeKitTests/BitBufferTests.swift
+++ Tests/QRCodeKitTests/BitBufferTests.swift
@@ -10,18 +10,17 @@ import Testing
 
 @Suite
 struct BitBufferTests {
-    
-    // MARK: - append(_:bitCount:)
-    
     @Test
     func emptyBitBuffer() {
         let buffer = BitBuffer()
         
         #expect(buffer.count == 0)
         #expect(buffer.byteCount == 0)
-        #expect(buffer.isAligned == true)
+        #expect(buffer.isAligned)
         #expect(buffer.bytes.isEmpty)
     }
+    
+    // MARK: - append(_:bitCount:)
 
     @Test
     func appendOneBit() {
@@ -29,10 +28,10 @@ struct BitBufferTests {
         
         buffer.append(1, bitCount: 1)
         
-        #expect(buffer.bytes == [0b10000000])
         #expect(buffer.count == 1)
         #expect(buffer.byteCount == 1)
         #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [0b10000000])
     }
 
     @Test
@@ -41,10 +40,10 @@ struct BitBufferTests {
         
         buffer.append(0, bitCount: 1)
         
-        #expect(buffer.bytes == [0b00000000])
         #expect(buffer.count == 1)
         #expect(buffer.byteCount == 1)
         #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [0b00000000])
     }
 
     @Test
@@ -53,10 +52,10 @@ struct BitBufferTests {
         
         buffer.append(0b101101, bitCount: 6)
         
-        #expect(buffer.bytes == [0b10110100])
         #expect(buffer.count == 6)
         #expect(buffer.byteCount == 1)
         #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [0b10110100])
     }
 
     @Test
@@ -65,10 +64,10 @@ struct BitBufferTests {
         
         buffer.append(0b10110110, bitCount: 8)
         
-        #expect(buffer.bytes == [0b10110110])
         #expect(buffer.count == 8)
         #expect(buffer.byteCount == 1)
         #expect(buffer.isAligned)
+        #expect(buffer.bytes == [0b10110110])
     }
 
     @Test
@@ -77,10 +76,13 @@ struct BitBufferTests {
         
         buffer.append(0b1011011010, bitCount: 10)
         
-        #expect(buffer.bytes == [0b10110110, 0b10000000])
         #expect(buffer.count == 10)
         #expect(buffer.byteCount == 2)
         #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [
+            0b10110110,
+            0b10000000
+        ])
     }
 
     @Test
@@ -89,10 +91,10 @@ struct BitBufferTests {
         
         buffer.append(0xDEADBEEF, bitCount: 32)
         
-        #expect(buffer.bytes == [0xDE, 0xAD, 0xBE, 0xEF])
         #expect(buffer.count == 32)
         #expect(buffer.byteCount == 4)
         #expect(buffer.isAligned)
+        #expect(buffer.bytes == [0xDE, 0xAD, 0xBE, 0xEF])
     }
 
     @Test
@@ -102,10 +104,10 @@ struct BitBufferTests {
         buffer.append(0b101, bitCount: 3)
         buffer.append(0b11001, bitCount: 5)
         
-        #expect(buffer.bytes == [0b10111001])
         #expect(buffer.count == 8)
         #expect(buffer.byteCount == 1)
         #expect(buffer.isAligned)
+        #expect(buffer.bytes == [0b10111001])
     }
 
     @Test
@@ -115,10 +117,13 @@ struct BitBufferTests {
         buffer.append(0b101101, bitCount: 6)
         buffer.append(0b1101, bitCount: 4)
         
-        #expect(buffer.bytes == [0b10110111, 0b01000000])
         #expect(buffer.count == 10)
         #expect(buffer.byteCount == 2)
         #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [
+            0b10110111,
+            0b01000000
+        ])
     }
 
     @Test
@@ -128,10 +133,13 @@ struct BitBufferTests {
         buffer.append(0b10110110, bitCount: 8)
         buffer.append(0b101, bitCount: 3)
         
-        #expect(buffer.bytes == [0b10110110, 0b10100000])
         #expect(buffer.count == 11)
         #expect(buffer.byteCount == 2)
         #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [
+            0b10110110,
+            0b10100000
+        ])
     }
 
     @Test
@@ -142,46 +150,49 @@ struct BitBufferTests {
         buffer.append(0b11001, bitCount: 5)
         buffer.append(0b10, bitCount: 2)
         
-        #expect(buffer.bytes == [0b10111001, 0b10000000])
         #expect(buffer.count == 10)
         #expect(buffer.byteCount == 2)
         #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [
+            0b10111001,
+            0b10000000
+        ])
     }
 
     @Test
-    func appendValueWithLeadingZeroes() {
+    func appendValueWithLeadingZeros() {
         var buffer = BitBuffer()
         
         buffer.append(0b101, bitCount: 8)
         
-        #expect(buffer.bytes == [0b00000101])
         #expect(buffer.count == 8)
         #expect(buffer.byteCount == 1)
         #expect(buffer.isAligned)
+        #expect(buffer.bytes == [0b00000101])
     }
 
     @Test
-    func appendValueWithLeadingZeroesToPartialByte() {
+    func appendValueWithLeadingZerosToPartialByte() {
         var buffer = BitBuffer()
         
         buffer.append(0b101, bitCount: 5)
         
-        #expect(buffer.bytes == [0b00101000])
         #expect(buffer.count == 5)
         #expect(buffer.byteCount == 1)
         #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [0b00101000])
     }
 
     @Test
-    func appendAllZeroes() {
+    func appendAllZeros() {
         var buffer = BitBuffer()
         
         buffer.append(0, bitCount: 8)
         
-        #expect(buffer.bytes == [0b00000000])
         #expect(buffer.count == 8)
         #expect(buffer.byteCount == 1)
         #expect(buffer.isAligned)
+        #expect(buffer.bytes == [0b00000000])
     }
 
     @Test
@@ -192,13 +203,48 @@ struct BitBufferTests {
         buffer.append(0b110, bitCount: 6)
         buffer.append(0b1, bitCount: 4)
         
-        #expect(buffer.bytes == [0b00100100, 0b01100001])
         #expect(buffer.count == 16)
         #expect(buffer.byteCount == 2)
         #expect(buffer.isAligned)
+        #expect(buffer.bytes == [
+            0b00100100,
+            0b01100001
+        ])
     }
     
-    //MARK: - append(contentsOf:)
+    // MARK: - append(_:)
+    
+    @Test
+    func appendByteToEmptyBuffer() {
+        var buffer = BitBuffer()
+        let byte: UInt8 = 0b10101100
+        
+        buffer.append(byte)
+        
+        #expect(buffer.count == 8)
+        #expect(buffer.byteCount == 1)
+        #expect(buffer.isAligned)
+        #expect(buffer.bytes == [0b10101100])
+    }
+    
+    @Test
+    func appendByteToUnalignedBuffer() {
+        var buffer = BitBuffer()
+        let byte: UInt8 = 0b11001010
+        
+        buffer.append(0b101, bitCount: 3)
+        buffer.append(byte)
+        
+        #expect(buffer.count == 11)
+        #expect(buffer.byteCount == 2)
+        #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [
+            0b10111001,
+            0b01000000
+        ])
+    }
+    
+    // MARK: - append(contentsOf:)
     
     @Test
     func appendEmptyToEmptyBuffer() {
@@ -208,6 +254,8 @@ struct BitBufferTests {
         buffer.append(contentsOf: other)
         
         #expect(buffer.count == 0)
+        #expect(buffer.byteCount == 0)
+        #expect(buffer.isAligned)
         #expect(buffer.bytes.isEmpty)
     }
     
@@ -220,6 +268,8 @@ struct BitBufferTests {
         buffer.append(contentsOf: other)
         
         #expect(buffer.count == 8)
+        #expect(buffer.byteCount == 1)
+        #expect(buffer.isAligned)
         #expect(buffer.bytes == [0b11001100])
     }
     
@@ -232,6 +282,8 @@ struct BitBufferTests {
         buffer.append(contentsOf: other)
         
         #expect(buffer.count == 3)
+        #expect(buffer.byteCount == 1)
+        #expect(buffer.isAligned == false)
         #expect(buffer.bytes == [0b10100000])
     }
     
@@ -244,6 +296,8 @@ struct BitBufferTests {
         buffer.append(contentsOf: other)
         
         #expect(buffer.count == 8)
+        #expect(buffer.byteCount == 1)
+        #expect(buffer.isAligned)
         #expect(buffer.bytes == [0b10101010])
     }
     
@@ -256,46 +310,9 @@ struct BitBufferTests {
         buffer.append(contentsOf: other)
         
         #expect(buffer.count == 3)
+        #expect(buffer.byteCount == 1)
+        #expect(buffer.isAligned == false)
         #expect(buffer.bytes == [0b10100000])
-    }
-    
-    @Test
-    func appendUnalignedToAlignedBuffer() {
-        var buffer = BitBuffer()
-        var other = BitBuffer()
-        
-        buffer.append(0b11001100, bitCount: 8)
-        other.append(0b101, bitCount: 3)
-        buffer.append(contentsOf: other)
-        
-        #expect(buffer.count == 11)
-        #expect(buffer.bytes == [0b11001100, 0b10100000])
-    }
-    
-    @Test
-    func appendAlignedToUnalignedBuffer() {
-        var buffer = BitBuffer()
-        var other = BitBuffer()
-        
-        buffer.append(0b101, bitCount: 3)
-        other.append(0b11001100, bitCount: 8)
-        buffer.append(contentsOf: other)
-        
-        #expect(buffer.count == 11)
-        #expect(buffer.bytes == [0b10111001, 0b10000000])
-    }
-    
-    @Test
-    func appendUnalignedToUnalignedBuffer() {
-        var buffer = BitBuffer()
-        var other = BitBuffer()
-        
-        buffer.append(0b101, bitCount: 3)
-        other.append(0b1101, bitCount: 4)
-        buffer.append(contentsOf: other)
-        
-        #expect(buffer.count == 7)
-        #expect(buffer.bytes == [0b10111010])
     }
     
     @Test
@@ -308,7 +325,82 @@ struct BitBufferTests {
         buffer.append(contentsOf: other)
         
         #expect(buffer.count == 16)
-        #expect(buffer.bytes == [0b11001100, 0b00110011])
+        #expect(buffer.byteCount == 2)
+        #expect(buffer.isAligned)
+        #expect(buffer.bytes == [
+            0b11001100,
+            0b00110011
+        ])
+    }
+    
+    @Test
+    func appendAlignedToUnalignedBuffer() {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        buffer.append(0b101, bitCount: 3)
+        other.append(0b11001100, bitCount: 8)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 11)
+        #expect(buffer.byteCount == 2)
+        #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [
+            0b10111001,
+            0b10000000
+        ])
+    }
+    
+    @Test
+    func appendUnalignedToAlignedBuffer() {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        buffer.append(0b11001100, bitCount: 8)
+        other.append(0b101, bitCount: 3)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 11)
+        #expect(buffer.byteCount == 2)
+        #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [
+            0b11001100,
+            0b10100000
+        ])
+    }
+    
+    @Test
+    func appendUnalignedToUnalignedBuffer() {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        buffer.append(0b101, bitCount: 3)
+        other.append(0b1101, bitCount: 4)
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 7)
+        #expect(buffer.byteCount == 1)
+        #expect(buffer.isAligned == false)
+        #expect(buffer.bytes == [0b10111010])
+    }
+    
+    @Test
+    func appendMultiByteBufferToUnalignedBuffer() {
+        var buffer = BitBuffer()
+        var other = BitBuffer()
+        
+        buffer.append(0b101, bitCount: 3)
+        other.append(0b1100110010110, bitCount: 13)
+        
+        buffer.append(contentsOf: other)
+        
+        #expect(buffer.count == 16)
+        #expect(buffer.byteCount == 2)
+        #expect(buffer.isAligned)
+        #expect(buffer.bytes == [
+            0b10111001,
+            0b10010110
+        ])
     }
     
     @Test(arguments: Array(1...32))
@@ -317,10 +409,13 @@ struct BitBufferTests {
         var other = BitBuffer()
         
         let value = UInt32.max >> (32 - bitCount)
+        
         other.append(value, bitCount: bitCount)
         buffer.append(contentsOf: other)
         
         #expect(buffer.count == bitCount)
+        #expect(buffer.byteCount == other.byteCount)
+        #expect(buffer.isAligned == other.isAligned)
         #expect(buffer.bytes == other.bytes)
     }
 }
